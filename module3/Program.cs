78accb41c51c8e0cd6f6cab0d946309b9ed9c792
Shavana Peynado <shavypeynado@gmail.com>
2025-01-17 14:03:26 +0000

    modules 2 - 4 are uploaded onto github

---
+++ module3/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Comparison operators
+// int age = 23;
+// Console.WriteLine(5==5);
+// Console.WriteLine(5>=5);
+
+//code along
+
+// int first = 23;
+// int second = 10;
+
+// Console.WriteLine(first==second);
+// Console.WriteLine(first>=second);
+// Console.WriteLine("ten"!=second);
+// Console.WriteLine(10==second);
+
+// Condtional statements
+using System.Runtime.CompilerServices;
+
+int age = 10;
+if (age >=18){
+Console.WriteLine("you are eligible to vote");
+}
+else
+{Console.WriteLine("you are not eligible to vote");
+};
+
+// Logical operators
+bool dog = true;
+bool cat = true;
+
+if (dog || cat) {
+    Console.WriteLine("You have choosen a dog or a cat!");
+} else {
+    Console.WriteLine("Please choose one..");
+}
+
+bool raining = false;
+if (!raining) {
+    Console.WriteLine("It's not raining");
+} else {
+    Console.WriteLine("It is raining!");
+}
---
+++ module4/Program.cs
@@ -0,0 +1,26 @@
+﻿// for loops
+// for (int i = 1; i<=5; i++) {
+//     Console.WriteLine(i);
+// }
+
+// while loops
+// int countdown = 5;
+// while (countdown > 3) {
+//     Console.WriteLine(countdown);
+//     countdown --;
+// }
+
+// infinite loops
+// int countdown = 5;
+// while (countdown > 3) {
+//     Console.WriteLine(countdown);
+//     countdown ++;
+// }
+
+// methods & functions
+// int AddNumbers (int firstNumber, int secondNumber) {
+//     int result = firstNumber - secondNumber;
+//     return result;
+// }
+//  int sum = AddNumbers(20,7);
+//  Console.WriteLine(sum);
